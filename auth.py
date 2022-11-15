5abc1f14547eb48d9b5b300f486e3917e888432e
Naveen Dharni <naveen.dharni@gmail.com>
2022-11-14 22:43:38 -0500

    Modifying the data

--- auth.py
+++ auth.py
@@ -1,10 +1,10 @@
 import tweepy
 
 # Authentication
-consumer_key = ""
-consumer_secret = ""
-access_token = ""
-access_token_secret = ""
+consumer_key = "lPG5nsVL1MLfPOXQYjiZWmMDy"
+consumer_secret = "BpD8OIY6bpY8BHnZdZq2UGedQ3HhNXUwX9kCvDhxt9cnTie93r"
+access_token = "388728828-eda22V9DbIiuk5XjXJ5gQrMVCXdNWTytYn3NvEMV"
+access_token_secret = "yTDb86ev1el1igWOGyz76wHamTWna0LsZAB9vVsVzeQbK"
 
 # Access Twitter Data
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
--- main.py
+++ main.py
@@ -1,54 +1,32 @@
-import re
-import numpy as np
 import tweepy
 from textblob import TextBlob
 import matplotlib.pyplot as plt
 import pandas as pd
 from wordcloud import WordCloud
-from better_profanity import profanity
+
 import auth
+import utils
 
-# Input a query from the user
-query = input("Please enter your topic / person of interest: ")
-
-# In this case, we will input the query as 'F1 Racing'
-# Filter the query to remove retweets
-filtered = query + "-filter:retweets"
+# Input a query from the user & filter to remove retweets
+query = input("Please enter a hashtag: ")
+actual_tweets = query + "-filter:retweets"
 
 # Generate the latest tweets on the given query
-tweets = tweepy.Cursor(auth.api.search_tweets, q=filtered, lang="en").items(100)
+tweets = tweepy.Cursor(auth.api.search_tweets, q=actual_tweets, lang="en").items(100)
 
 # Create a list of the tweets, the users, and their location
 list1 = [[tweet.text, tweet.user.screen_name, tweet.user.location] for tweet in tweets]
 
 # Convert the list into a dataframe
-df = pd.DataFrame(data=list1, columns=['tweets','user', "location"])
+df = pd.DataFrame(data=list1, columns=['tweets', 'user', "location"])
 
 # Convert only the tweets into a list
 tweet_list = df.tweets.to_list()
 
-# Create a function to clean the tweets. Remove profanity, unnecessary characters, spaces, and stopwords.
-def clean_tweet(tweet):
-    if type(tweet) == np.float:
-        return ""
-    r = tweet.lower()
-    r = profanity.censor(r)
-    r = re.sub("'", "", r) # This is to avoid removing contractions in english
-    r = re.sub("@[A-Za-z0-9_]+","", r)
-    r = re.sub("#[A-Za-z0-9_]+","", r)
-    r = re.sub(r'http\S+', '', r)
-    r = re.sub('[()!?]', ' ', r)
-    r = re.sub('\[.*?\]',' ', r)
-    r = re.sub("[^a-z0-9]"," ", r)
-    r = r.split()
-    stopwords = ["for", "on", "an", "a", "of", "and", "in", "the", "to", "from"]
-    r = [w for w in r if not w in stopwords]
-    r = " ".join(word for word in r)
-    return r
-
-# Run the list of tweets through the function.
-cleaned = [clean_tweet(tw) for tw in tweet_list]
+# Data cleaning
+cleaned = [utils.clean_tweet(tw) for tw in tweet_list]
 print("List of tweet", cleaned)
+print("Number of tweet", len(cleaned))
 
 # Define the sentiment objects using TextBlob
 sentiment_objects = [TextBlob(tweet) for tweet in cleaned]
